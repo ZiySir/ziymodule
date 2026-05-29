8e0f2280d391c71786e43111981a47b78c6b4d0f
ziy <@>
2026-05-29 13:39:27 +0800

    data模块基本搭建

--- module/data/src/main/java/me/ziyframework/module/data/converter/DurationToSecondsConverter.java
+++ module/data/src/main/java/me/ziyframework/module/data/converter/DurationToSecondsConverter.java
@@ -6,11 +6,18 @@ import java.time.Duration;
 
 @Converter
 public class DurationToSecondsConverter implements AttributeConverter<Duration, Long> {
+
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public Long convertToDatabaseColumn(Duration attribute) {
         return attribute.toSeconds();
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public Duration convertToEntityAttribute(Long dbData) {
         return Duration.ofSeconds(dbData);
